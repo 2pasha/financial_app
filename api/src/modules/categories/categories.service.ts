fd28f9b66fe33c9d52afd90b27c42f81a09feb40
2pasha <kostishinpavlo24@gmail.com>
2026-03-27 23:11:40 +0200

    fixed dashboard calculations

--- api/src/modules/categories/categories.service.ts
+++ api/src/modules/categories/categories.service.ts
@@ -24,7 +24,6 @@ export class CategoriesService {
         transactions: {
           select: { amount: true },
           where: {
-            hold: false,
             time: { gte: fromDate, lte: toDate },
           },
         },
@@ -152,7 +151,6 @@ export class CategoriesService {
       where: {
         categoryId,
         userId: user.id,
-        hold: false,
         time: { gte: fromDate, lte: toDate },
       },
       orderBy: { time: 'desc' },
